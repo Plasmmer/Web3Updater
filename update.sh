5f5190d44170fa897e67e6ffa0544956bb6f73cc
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-05-02 19:19:02 -0300

    Commit

--- update.sh
+++ update.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo ""
+if [ $(jq -r '.patch_at' patch_at.json) = "0" ]; then
+   echo "Installing update 1..."
+   echo "This is a thing that the first update did." > patch_1.txt
+   jq '.patch_at = "1"' patch_at.json > "$tmp" && mv "$tmp" patch_at.json
+fi
 
+if [ $(jq -r '.patch_at' patch_at.json) = "1" ]; then
+   echo "Installing update 2..."
+   echo "This is another effect made by an update." > another_effect.txt
+   jq '.patch_at = "2"' patch_at.json > "$tmp" && mv "$tmp" patch_at.json
+fi
