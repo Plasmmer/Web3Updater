f36afaa0370b123cb5ddfce11b9fa713f38a6ec4
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-05-02 18:37:41 -0300

    add update.sh and update.json

--- web3updater.sh
+++ web3updater.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-echo $(ethereal ens contenthash get --domain=plasmmer.eth)
+echo "Welcome to Web3Updater!"
+echo ""
+echo "The web3 (that came from Ethereum and IPFS) is replacing the web 2.0, and Web3Updater will put it also on OS/app updates!"
+echo ""
+echo "Never more depend on centralized servers to get updates."
+echo ""
+echo "As Web3Updater can be used in any product to manage updates (such as Electron apps), it uses itself to update itself (meta-language)."
+echo ""
+echo "Updating Web3Updater..."
 
 git clone https://ipfs.io$(ethereal ens contenthash get --domain=plasmmer.eth)
 
