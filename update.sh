b1fc6c72c1b3283ac83bdc168a67c88a42a1a9ab
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-05-03 21:29:52 -0300

    refactor

--- update.sh
+++ update.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-if [ $(jq -r '.patch_at' patch_at.json) = "0" ]; then
+if [ $(jq -r '.patch_at' update.json) = "0" ]; then
    echo "Installing update 1..."
    echo "This is a thing that the first update did." > patch_1.txt
-   contents="$(jq '.patch_at = "1"' patch_at.json)" && \
-echo "${contents}" > patch_at.json
+   contents="$(jq '.patch_at = "1"' update.json)" && \
+echo "${contents}" > update.json
 fi
 
-if [ $(jq -r '.patch_at' patch_at.json) = "1" ]; then
+if [ $(jq -r '.patch_at' update.json) = "1" ]; then
    echo "Installing update 2..."
    echo "This is another effect made by an update." > another_effect.txt
-   contents="$(jq '.patch_at = "2"' patch_at.json)" && \
-echo "${contents}" > patch_at.json
+   contents="$(jq '.patch_at = "2"' update.json)" && \
+echo "${contents}" > update.json
 fi
+
+#if [ $(jq -r '.patch_at' update.json) = "2" ]; then
+#   echo "Installing update 3..."
+#   rm patch_1.txt
+#   rm another_effect.txt
+#   jq '. += ["cid_at": ""]' update.json
+#   contents="$(jq '.patch_at = "3"' update.json)" && \
+#echo "${contents}" > update.json
+#fi
--- web3updater.sh
+++ web3updater.sh
@@ -2,8 +2,9 @@
 
 echo "Welcome to Web3Updater!"
 echo "Made in a day where my mom couln't sleep because of an Hare Krishna that lives with my uncle, and the whole day she yelled at us and I became confused. Half day building this piece of art+utility, half day reasoning poorly and thinking to burn all my crypto because money != peace in some cases. But I won't burn my tokens because there are my mom, my uncle and my sister counting on me, and I hope I can change the life of more people, if I even can change the life of my mom and myself/; which is being hard. My brain bugged now to think more so I'll try to focus back on outbursting by building this heresy that no one of these machist transphobe beared professional programmers have thought of. I know Im writing like in 2019 and this is an backstep, but my brain really bugged today, my rare peace of mind from a whole month has been partly broken today. My mom is bipolar, and about Hare I don't want to judge while irritated."
+echo "Made in Itajai, Santa Catarina. IMPORTANT: this is not an Brazilian funk music remix."
 
-if [ ! -f patch_at.json ]; then
+if [ ! -f update.json ]; then
     echo ""
     echo "It looks like its your first time using Web3Updater..."
     echo ""
@@ -14,7 +15,7 @@ echo ""
 echo "As Web3Updater can be used in any product to manage updates (such as Electron apps), it uses itself to update itself (meta-language)."
 echo ""
 echo "Initializing..."
-    cat > patch_at.json <<EOF
+    cat > update.json <<EOF
 {
 "patch_at": 0
 }
@@ -23,15 +24,14 @@ echo "Done!"
 fi
 
 echo ""
-echo "Version: v1 / Patch: $(jq -r '.patch_at' patch_at.json)"
+echo "Version: v1 / Patch: $(jq -r '.patch_at' update.json)"
 echo ""
 echo "Checking for new updates..."
 
-echo "$(ethereal ens contenthash get --domain=update.updating.eth)" #remove "/"
 #git remote add web3updater https://ipfs.io$(ethereal ens contenthash get --domain=update.updating.eth)
 #git pull web3updater
 
-if [ "$(jq -r '.new_patch_at' new_patch_at.json)" -gt "$(jq -r '.patch_at' patch_at.json)" ]; then
+if [ "$(jq -r '.new_patch_at' new_patch_at.json)" -gt "$(jq -r '.patch_at' update.json)" ]; then
     echo "Updating Web3Updater..."
     sh ./update.sh
 else
@@ -40,6 +40,6 @@ fi
 
 #- install IPFS if not detected (mark it as a template feature for Floflis installer)
 #- install ipget if not detected
-#- ipfs ls /ipfs/QmYrS7cNYXGGh4n38w5vcgBrinF7X3Yii24PCa2FEWVXuZ/
-#- ipfs get --output=tmp-remote QmYrS7cNYXGGh4n38w5vcgBrinF7X3Yii24PCa2FEWVXuZ
-#- ipfs pin add QmYrS7cNYXGGh4n38w5vcgBrinF7X3Yii24PCa2FEWVXuZ
+#- ipfs ls $(ethereal ens contenthash get --domain=update.updating.eth)
+#- ipfs get --output=tmp-remote $(ethereal ens contenthash get --domain=update.updating.eth)
+#- ipfs pin add $(ethereal ens contenthash get --domain=update.updating.eth)
