0b9452de39bf7966a8128216e050796eed93e4c8
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-05-07 18:56:30 -0300

    install on /libs folder like Floflis, with its /bin exec

--- update.sh
+++ update.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-if [ $(jq -r '.patch_at' update.json) = "0" ]; then
+if [ $(jq -r '.patch_at' /usr/lib/web3updater/update.json) = "0" ]; then
    echo "Installing update 1..."
-   echo "This is a thing that the first update did." > patch_1.txt
-   contents="$(jq '.patch_at = "1"' update.json)" && \
-   echo "${contents}" > update.json
+   echo "This is a thing that the first update did." > /usr/lib/web3updater/patch_1.txt
+   contents="$(jq '.patch_at = "1"' /usr/lib/web3updater/update.json)" && \
+   echo "${contents}" > /usr/lib/web3updater/update.json
 fi
 
-if [ $(jq -r '.patch_at' update.json) = "1" ]; then
+if [ $(jq -r '.patch_at' /usr/lib/web3updater/update.json) = "1" ]; then
    echo "Installing update 2..."
-   echo "This is another effect made by an update." > another_effect.txt
-   contents="$(jq '.patch_at = "2"' update.json)" && \
-   echo "${contents}" > update.json
+   echo "This is another effect made by an update." > /usr/lib/web3updater/another_effect.txt
+   contents="$(jq '.patch_at = "2"' /usr/lib/web3updater/update.json)" && \
+   echo "${contents}" > /usr/lib/web3updater/update.json
 fi
 
-if [ $(jq -r '.patch_at' update.json) = "2" ]; then
+if [ $(jq -r '.patch_at' /usr/lib/web3updater/update.json) = "2" ]; then
    echo "Installing update 3..."
-   rm patch_1.txt
-   rm another_effect.txt
-   cat update.json | jq '. + {"cid_at": ""}' | tee update.json
-   contents="$(jq '.patch_at = "3"' update.json)" && \
-   echo "${contents}" > update.json
+   sudo rm /usr/lib/web3updater/patch_1.txt
+   sudo rm /usr/lib/web3updater/another_effect.txt
+   cat /usr/lib/web3updater/update.json | jq '. + {"cid_at": ""}' | tee /usr/lib/web3updater/update.json
+   contents="$(jq '.patch_at = "3"' /usr/lib/web3updater/update.json)" && \
+   echo "${contents}" > /usr/lib/web3updater/update.json
 fi
 
-if [ $(jq -r '.patch_at' update.json) = "3" ]; then
+if [ $(jq -r '.patch_at' /usr/lib/web3updater/update.json) = "3" ]; then
    echo "Installing update 4..."
-   cat update.json | jq '. + {"recently_checked_at": ""}' | tee update.json
-   cat update.json | jq '. + {"recently_updated_at": ""}' | tee update.json
-   contents="$(jq '.patch_at = "4"' update.json)" && \
-   echo "${contents}" > update.json
+   cat /usr/lib/web3updater/update.json | jq '. + {"recently_checked_at": ""}' | tee /usr/lib/web3updater/update.json
+   cat /usr/lib/web3updater/update.json | jq '. + {"recently_updated_at": ""}' | tee /usr/lib/web3updater/update.json
+   contents="$(jq '.patch_at = "4"' /usr/lib/web3updater/update.json)" && \
+   echo "${contents}" > /usr/lib/web3updater/update.json
 fi
