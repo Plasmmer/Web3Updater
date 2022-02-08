a87d46fcc6ed34e5a18195f1711e545e0f0898ac
Daniell Mesquita <daniellmesquita@protonmail.com>
2022-02-07 21:02:11 -0300

    Make it simpler by leaving from-updates functions to a separate script

---
+++ sample_updates_3-5.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ "$1" = "apply3" ]; then
+   if [ $(jq -r '.patch_at' /usr/lib/web3updater/update.json) -ge 3 ]; then
+       contents1="$(jq -r '.ens' /usr/lib/web3updater/update.json)"
+       cidat=$(ethereal ens contenthash get --domain=${contents1})
+       contents="$(jq ".cid_at = \"$cidat\"" /usr/lib/web3updater/update.json)" && \
+       echo "${contents}" > /usr/lib/web3updater/update.json
+fi
+fi
+
+if [ "$1" = "apply4" ]; then
+   # --------------##########---------------------- This checks for update 4 and does its specific actions
+   if [ $(jq -r '.patch_at' /usr/lib/web3updater/update.json) -ge "4" ]; then
+      recentlycheckedat=$(date +"%s")
+      contents="$(jq ".recently_checked_at = \"$recentlycheckedat\"" /usr/lib/web3updater/update.json)" && \
+      echo "${contents}" > /usr/lib/web3updater/update.json
+      
+      if [ "$2" = "recordrecently" ]; then
+         recentlyupdatedat=$(date +"%s")
+         contents="$(jq ".recently_updated_at = \"$recentlyupdatedat\"" /usr/lib/web3updater/update.json)" && \
+         echo "${contents}" > /usr/lib/web3updater/update.json
+fi
+      
+      if [ "$2" = "displayrecently" ]; then
+         contents=$(jq -r '.recently_checked_at' /usr/lib/web3updater/update.json)
+         echo "Recently checked at: $(date -d @${contents})"
+         contents=$(jq -r '.recently_updated_at' /usr/lib/web3updater/update.json)
+         echo "Recently updated at: $(date -d @${contents})"
+fi
+      
+fi
+fi
+
+if [ "$1" = "apply5" ]; then
+   if [ $(jq -r '.patch_at' /usr/lib/web3updater/update.json) -ge "5" ]; then
+      #- check if cid is different before continuing, thats what "Checking for updates" should work for
+      contents="$(jq -r '.ens' /usr/lib/web3updater/update.json)"
+      echo "Contents is # $contents #"
+      ipfs pin add $(ethereal ens contenthash get --domain=${contents})
+      ipfs ls $(ethereal ens contenthash get --domain=${contents})
+      ipfs get --output=/usr/lib/web3updater/tmp-remote $(ethereal ens contenthash get --domain=${contents})
+      cd /usr/lib/web3updater && git pull web3updater main && git remote remove web3updater && rm -rf /usr/lib/web3updater/tmp-remote && cd "$SCRIPTPATH"
+fi
+fi
